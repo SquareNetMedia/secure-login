4437d7771942b1e27892fa297b4d3877e0191b89
SquareNetMedia <alexjameshenderson11@gmail.com>
2021-03-13 09:25:43 +0000

    Updated 13.03.21

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => 'web'], function () {
 });
 
-Route::get('/a', function () {
-   return secure-login::config.url;
+
+Route::group(array('prefix' => Config::get('secure-login.url')), function ()
+{
+
+   Route::get('/a', function () {
+      
+      //check if the config file is loaded - not sure if this is required now
+      if (is_null(config('secure-login'))){
+         return ('Please publish the config file by running ' .
+         'php artisan vendor:publish --tag=secure-login-config\'');
+      }
+
+      return config('secure-login.url');
+
+   });
+
 });
--- src/SecureLoginServiceProvider.php
+++ src/SecureLoginServiceProvider.php
@@ -22,7 +22,7 @@ class SecureLoginServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('secure-login.php'),
-            ], 'config');
+            ], 'secure-login-config');
 
             // Publishing the views.
             $this->publishes([
