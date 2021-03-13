cefb63479c2cc71868e008aebf462a3813bdd254
SquareNet Media <ajh@squarenetmedia.com>
2021-03-13 12:00:20 +0000

    Apply fixes from StyleCI (#7)

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,26 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 
 Route::group(
    [
-      'middleware' => 'web', 
-      'prefix' => Config::get('secure-login.url')
+       'middleware' => 'web',
+       'prefix' => Config::get('secure-login.url'),
    ], function () {
-
-      Route::get('/alex', function () {
-         return "secured";
-      });
-});
-
-
-
-Route::group(array('prefix' => Config::get('secure-login.url')), function ()
-{
-
-   Route::get('/a', function () {
-      
-      //check if the config file is loaded - not sure if this is required now
-      if (is_null(config('secure-login'))){
-         return ('Please publish the config file by running ' .
-         'php artisan vendor:publish --tag=secure-login-config\'');
-      }
-
-      return config('secure-login.url');
-
+       Route::get('/alex', function () {
+           return 'secured';
+       });
    });
 
+Route::group(['prefix' => Config::get('secure-login.url')], function () {
+    Route::get('/a', function () {
+
+      //check if the config file is loaded - not sure if this is required now
+        if (is_null(config('secure-login'))) {
+            return 'Please publish the config file by running '.
+         'php artisan vendor:publish --tag=secure-login-config\'';
+        }
+
+        return config('secure-login.url');
+    });
 });
